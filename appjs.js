c506a0d74749c651d00a335fac3eda8520204174
ntompkins <ntompkins@classworks.com>
2019-11-07 16:55:16 -0600

    Finished according to instructions.

--- appjs.js
+++ appjs.js
@@ -1,1 +1,17 @@
 let friends = [];
+
+friends = ["Bill", "Ruth", "Jon", "Mike", "Heidi"];
+let output = "";
+let line = " lines";
+let beginning = " of code in the file, ";
+let middleOne = " of code; ";
+let middleTwo = " strikes one out, clears it all out, ";
+let end = " of code in the file";
+
+for (let i = 0; i < friends.length; i++) {
+  console.log(friends[i].toUpperCase());
+  for (let j = 99; j > 0; j--) {
+    output = j + " " + ((j > 1) ? "lines" : "line") + beginning + j + ((j > 1) ? " lines" : " line") + middleOne + friends[i] + middleTwo + ((j > 1) ? j + line : "no more lines") + end;
+    console.log(output);
+  }
+}
