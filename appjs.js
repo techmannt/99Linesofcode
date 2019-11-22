3cae670fb877ad5bef71aa46ffb1eefe20d71d49
ntompkins <ntompkins@classworks.com>
2019-11-22 08:59:27 -0600

    Fixed error with numbering, made code more efficient, and used short-circuiting according to review.

--- appjs.js
+++ appjs.js
@@ -2,16 +2,19 @@ let friends = [];
 
 friends = ["Bill", "Ruth", "Jon", "Mike", "Heidi"];
 let output = "";
-let line = " lines";
 let beginning = " of code in the file, ";
 let middleOne = " of code; ";
 let middleTwo = " strikes one out, clears it all out, ";
 let end = " of code in the file";
 
-for (let i = 0; i < friends.length; i++) {
-  console.log(friends[i].toUpperCase());
+for (let friend of friends) {
+  console.log(friend.toUpperCase());
   for (let j = 99; j > 0; j--) {
-    output = j + " " + ((j > 1) ? "lines" : "line") + beginning + j + ((j > 1) ? " lines" : " line") + middleOne + friends[i] + middleTwo + ((j > 1) ? j + line : "no more lines") + end;
+    output = j + (lines(j)) + beginning + j + (lines(j)) + middleOne + friend + middleTwo + (j - 1 || 'no more') + lines(j - 1) + end;
     console.log(output);
   }
+}
+
+function lines(n) {
+  return n === 1 ? ' line' : ' lines';
 }
